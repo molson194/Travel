26b219bac0df117d4595273bfb47b13786594597
Matthew Olson <Olson@Matthews-MacBook-Air-2.local>
2016-05-29 14:40:19 -0600

    Added South Island Trip

--- js/custom.js
+++ js/custom.js
@@ -2,38 +2,6 @@
 $(window).load(function(){
     $('.preloader').fadeOut("slow"); // set duration in brackets    
 });
-// ----- GOOGLE MAP ----- //
-var map = '';
-var center;
-
-function initialize() {
-    var mapOptions = {
-      zoom: 16,
-      center: new google.maps.LatLng(13.758468, 100.567481),
-      scrollwheel: false
-    };
-  
-    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
-
-    google.maps.event.addDomListener(map, 'idle', function() {
-        calculateCenter();
-    });
-  
-    google.maps.event.addDomListener(window, 'resize', function() {
-        map.setCenter(center);
-    });
-}
-
-function calculateCenter() {
-  center = map.getCenter();
-}
-
-function loadGoogleMap(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
-}
 
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
@@ -55,9 +23,6 @@ $(function(){
     mobile: false
   });
   wow.init();
-
-  // ------- GOOGLE MAP ----- //
-  loadGoogleMap();
 
   // ------- JQUERY PARALLAX ---- //
   function initParallax() {
